c3db48fad35563e459a63834131eb459ccca632c
sidharth nayyar <sidnayyar1@gmail.com>
2019-06-22 17:03:23 -0400

    Initial Commit

---
+++ LibraryManagementSystemClone/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LibraryManagementSystemClone
+//
+//  Created by MacStudent on 2019-06-22.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
